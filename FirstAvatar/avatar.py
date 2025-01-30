74a381353a0f529abbe25c40044742f35ff7d2ed
Alexis Meneses <alexis.meneses@irl.sys.es.osaka-u.ac.jp>
2025-01-31 00:09:34 +0900

    adding text

--- FirstAvatar/avatar.py
+++ FirstAvatar/avatar.py
@@ -8,24 +8,29 @@ client = OpenAI(
 
 
 root = Tk()
+
+respuestaDeGPT = StringVar()
+input_text = StringVar() 
+frm = ttk.Frame(root, padding=100)
+frm.grid()
+ttk.Label(frm, text="Mi primer chat bot").grid(column=0, row=0)
+EntradaTexto = ttk.Entry(frm, textvariable = input_text, justify = CENTER).grid(column=0, row=2) 
+textoDeCharla = ttk.Label(frm,textvariable=respuestaDeGPT).grid(column=0, row=4)
+
+
 def hablar():
     response = client.chat.completions.create(
                         model = "gpt-3.5-turbo",
                         messages = [
                             {"role": "system", "content": "Cada vez que te de un nombre, quiero que describas cualidades hermosas y fascinantes en base a ese nombre."},
-                            {"role": "user", "content": "presentate"}
+                            {"role": "user", "content": input_text.get()}
                         ],
                         temperature=0
                 )
     text = response.choices[0].message.content.strip()
     respuestaDeGPT.set(text)
-respuestaDeGPT = StringVar()
-frm = ttk.Frame(root, padding=100)
-frm.grid()
-ttk.Label(frm, text="Mi primer chat bot").grid(column=0, row=0)
-textoDeCharla = ttk.Label(frm,textvariable=respuestaDeGPT).grid(column=0, row=2)
 
-ttk.Button(frm, text="Enviar", command=hablar).grid(column=0, row=4)
+ttk.Button(frm, text="Enviar", command=hablar).grid(column=0, row=6)
 root.mainloop()
 
 
